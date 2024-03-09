b5b03ba7d684461ce8f1653c43439b26cdd893e5
0xdev <zinzuvadiyameet98@gmail.com>
2024-03-09 14:19:22 +0530

    solve error while deploying

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,16 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
 import { config } from "./service/wagmi.conf.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
-import Peer2 from "./screen/Sessions.tsx";
+import App from "./App.tsx";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <WagmiProvider config={config}>
     <QueryClientProvider client={queryClient}>
-      <Peer2 />
+      <App />
     </QueryClientProvider>
   </WagmiProvider>
 );
