63c481ef45d1288b19fb8ac82810394ba15e5cd7
0xdev <zinzuvadiyameet98@gmail.com>
2024-03-10 14:03:37 +0530

    update turn server

--- src/service/peer.config.tsx
+++ src/service/peer.config.tsx
@@ -1,27 +1,12 @@
 export const peerConnection = new RTCPeerConnection({
   iceServers: [
     {
-      urls: "stun:stun.relay.metered.ca:80",
+      urls: "stun:174.138.123.102:3478",
     },
     {
-      urls: "turn:asia.relay.metered.ca:80",
-      username: "a6d24450d0bb94535a638bf9",
-      credential: "z+a2xBF8Guz8LkSF",
-    },
-    {
-      urls: "turn:asia.relay.metered.ca:80?transport=tcp",
-      username: "a6d24450d0bb94535a638bf9",
-      credential: "z+a2xBF8Guz8LkSF",
-    },
-    {
-      urls: "turn:asia.relay.metered.ca:443",
-      username: "a6d24450d0bb94535a638bf9",
-      credential: "z+a2xBF8Guz8LkSF",
-    },
-    {
-      urls: "turns:asia.relay.metered.ca:443?transport=tcp",
-      username: "a6d24450d0bb94535a638bf9",
-      credential: "z+a2xBF8Guz8LkSF",
+      urls: "turn:174.138.123.102:3478",
+      username: "domegleturn",
+      credential: "cytbow-zimnij-Werju2",
     },
   ],
 });
