d1dcc760eb1a45fd0507220018d90c1c4793b7f0
0xdev <zinzuvadiyameet98@gmail.com>
2024-03-11 13:03:30 +0530

    update turn and stun servers

--- src/service/peer.config.tsx
+++ src/service/peer.config.tsx
@@ -1,10 +1,15 @@
 export const peerConnection = new RTCPeerConnection({
   iceServers: [
     {
-      urls: "stun:174.138.123.102:3478",
+      urls: "stun:68.183.91.117:3478",
     },
     {
-      urls: "turn:174.138.123.102:3478",
+      urls: "turn:68.183.91.117:3478",
+      username: "domegleturn",
+      credential: "cytbow-zimnij-Werju2",
+    },
+    {
+      urls: "turn:68.183.91.117:3478?transport=tcp",
       username: "domegleturn",
       credential: "cytbow-zimnij-Werju2",
     },
