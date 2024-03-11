11e24f8c623b6366ea3f72483c42637cdf176db7
0xdev <zinzuvadiyameet98@gmail.com>
2024-03-11 20:28:06 +0530

    remove commented code due to deployment error

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,5 @@
-// import { useAccount } from "wagmi";
-// import Peer from "./screen/peer";
-// import Home from "./screen/Home";
-// import { polygonZkEvmTestnet } from "wagmi/chains";
 import { Outlet } from "react-router";
-// import Peer2 from "./screen/Peer2";
 
 export default function App() {
-  // const { isConnected } = useAccount();
-  // const account = useAccount();
-
   return <Outlet />;
-
-  // if (window.ethereum) {
-  //   if (isConnected && account.chainId === polygonZkEvmTestnet.id)
-  //     return <Peer />;
-  //   return <Home />;
-  // } else {
-  //   return (
-  //     <div className="flex w-full text-center justify-center mt-20">
-  //       <p className="w-1/3 text-lg">
-  //         😢 It seems like your browser does not support the crypto wallet
-  //         extension. Please{" "}
-  //         <a
-  //           className="font-bold text-blue-500"
-  //           href="https://metamask.io/download/"
-  //         >
-  //           install
-  //         </a>{" "}
-  //         the extension to proceed.
-  //       </p>
-  //     </div>
-  //   );
-  // }
 }
